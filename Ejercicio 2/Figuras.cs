25ebf61088fd298ade5b59d411daf4dffa9e0ed4
abelvaazquez98@gmail.com <abelvaazquez98@gmail.com>
2020-12-10 20:53:17 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio 2/Figuras.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_2
+{
+    public class Figuras
+    {
+        double lado = 0;
+        double radio = 0;
+        double ancho = 0; 
+        double area = 0;
+
+        //para crear una función o m´ñetodo tenemos que añadir la visibilidad, seguida del tipo de
+       //valor que va a devolver o retornar nuestra función (si no devuelve nada es de tipo void), más el nombre de la función seguida de los
+       //argumentos que tengan y se cierra con las llaves.
+
+        public void areaCuadrado()
+        {
+
+            Console.WriteLine("Ingresa el valor del lado de un cuadrado cualquiera:");
+            lado = Convert.ToDouble(Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("El área del cuadrado es:" + area);
+        }
+
+        public void areaCirculo()
+        {
+
+            Console.WriteLine("Ingresa el valor del radio de un circulo cualquiera:");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI*Math.Pow(radio,2);
+            Console.WriteLine("El área del circulo es:" + area);
+        }
+
+        public void areaRectangulo()
+        {
+
+            Console.WriteLine("Ingresa el valor de la base de un rectangulo cualquiera:");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa el valor de la base de un rectangulo cualquiera:");
+            ancho = Convert.ToDouble(Console.ReadLine());
+            area = lado * ancho;
+            Console.WriteLine("El área del rectángulo es: " + area);
+        }
+    }
+}
---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Figuras figuras = new Figuras(); // Instanciar clase figuras
+                int contador = 1;
+                for (int i = 0; i < contador; i++)
+                {
+                    Console.WriteLine("¿Desea calcular el área de una figura? S/N");
+                    string opcion = Console.ReadLine();
+                    if (opcion.ToUpper() == "S")
+                    {
+                        contador++;
+                        Console.WriteLine("Elija una figura para calcular su area: \n" +
+                            "1.Área de un cuadrado \n" + "2.Área de un círculo \n" + "3. Área de un rectángulo");
+                        char figurasElegida = Convert.ToChar(Console.ReadLine());
+
+                        switch (figurasElegida)
+                        {
+                            case '1': figuras.areaCuadrado(); break;
+                            case '2': figuras.areaCirculo(); break;
+                            case '3': figuras.areaRectangulo(); break;
+                            default:
+                                Console.WriteLine("Opción no válida");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gracias por utilizar la aplicación");
+                        Console.WriteLine("     3...");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("     2...");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("     1...");
+                        System.Threading.Thread.Sleep(1000);
+                        return;
+                    }
+                }
+
+               
+
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Ha ocurrido un error inesperado. Error: " + ex.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
